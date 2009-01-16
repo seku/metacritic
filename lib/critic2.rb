e5d64cfca53a5094cdf86b66774afbaa11a10837
seku <sekupyna@gmail.com>
2009-01-16 09:26:36 +0100

    good looking

--- lib/critic2.rb
+++ lib/critic2.rb
@@ -3,22 +3,20 @@ require 'hpricot'
 require 'open-uri'
 
 module Score 
-
 	def score(new,name)
-				
-				h = {'title: ' => "#{name}"}
-				doc = Hpricot(open("http://www.metacritic.com/video/titles/#{new}"))
-				doc.search("table#scoretable//img") do |currency|
-					if currency.attributes['height'] == '50' && currency.attributes['width'] == '130'
-						h[' Critics: '] = "#{currency.attributes['alt'].delete("Metascore: ")}"			
-					end
-				end
-				doc.search("table#scoretable//td") do |currency|
-					if currency.attributes['class'] 
-						h[' Audience: '] = "#{currency.inner_html}"
-					end
-				end
-				h
+		h = {'title: ' => "#{name}"}
+		doc = Hpricot(open("http://www.metacritic.com/video/titles/#{new}"))
+		doc.search("table#scoretable//img") do |currency|
+			if currency.attributes['height'] == '50' && currency.attributes['width'] == '130'
+				h[' Critics: '] = "#{currency.attributes['alt'].delete("Metascore: ")}"			
+			end
+		end
+		doc.search("table#scoretable//td") do |currency|
+			if currency.attributes['class'] 
+				h[' Audience: '] = "#{currency.inner_html}"
+			end
+		end
+		h
 	end
 
 	def convert(name)
@@ -30,32 +28,24 @@ module Score
 		name = name.delete("+")
 		name = name.delete(":")
 		name = name.delete("-")
-		
-		#name = name.delete("+-: ")
 		while name.include? ","
 			name = name.chop
 		end
-
-
 		name
-
 	end
 
-
 	def critic(name)
-		
 		if name.include? " "
 			name = name.gsub(/ /,'+')
 		end
 		@i = 0
 		@table=[]
 		wynik = []
-			doc = Hpricot(open("http://www.metacritic.com/search/process?ts=#{name}&ty=1"))
-			doc.search("td#rightcolumn//a/b") do |current|
-				@table << current.inner_html
-				@i += 1
-			end
-		
+		doc = Hpricot(open("http://www.metacritic.com/search/process?ts=#{name}&ty=1"))
+		doc.search("td#rightcolumn//a/b") do |current|
+			@table << current.inner_html
+			@i += 1
+		end
 		if @i == 0
 			raise(ArgumentError, "There's no film , try again")
 		elsif @i > 10 
@@ -68,7 +58,6 @@ module Score
 			wynik	
 		end
 	end 
-
 	module_function:convert, :score, :critic
  
 end
